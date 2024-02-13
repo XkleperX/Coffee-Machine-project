de458e8cfa567f1ed1623535411da1afb4e4ec8f
Myhannad Alsrahen <mohanadalsrahenm1029@gmail.com>
2024-02-13 06:28:44 -0800

    modify on the code

--- main.py
+++ main.py
@@ -1,95 +1,88 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    },
-}
+from menu import MENU
+from art import logo,coin
+
+
+print(logo)
 resources = {"water": 300, "milk": 200, "coffee": 100, "money": 0}
 
-drink = input("What would you like? (espresso/latte/cappuccino):")
-if drink == "report":
-    print(
-        f"Water: {resources['water']}ml \nMilk: {resources['milk']}ml \ncoffee: {resources['coffee']}ml \nMoney: {resources['money']}"
-    )
-
-
-
-
-#! I need to get the order and get the amount of the wasted resources
-
-    
-#! or I can use the drink as this Menu["Drink"]["cost"] and save every thing done
-    
-
-
-def order(drink):
-    water = MENU[drink]["ingredients"]["water"]
-    milk = MENU[drink]["ingredients"]["milk"]
-    coffee = MENU[drink]["ingredients"]["coffee"]
-    cost = MENU[drink]["cost"]
-
-    return water,milk,coffee,cost
-
-print(order(drink))
-
-
-water_drink=order(drink)[0]
-milk_drink= order(drink)[1]
-coffee_drink= order(drink)[2]
-cost_drink= order(drink)[3]
-
-def check_order (water,milk,coffee,cost):
-    if water > resources["water"]:
-        print("you don't have enough water")
-    elif milk > resources["milk"]:
-        print("you don't have enough milk")
-    elif coffee > resources["coffee"]:
-        print("you don't have enough coffee")
-    elif money_count()<cost:
-        print("you don't have enough money")
-
-
-check_order(water=water_drink,milk=milk_drink,coffee=coffee_drink,cost=cost_drink)
-
-
-
-
-print("please insert a coin first")
 quarter = 0.25
 dimes = 0.10
 nickel = 0.05
 pennies = 0.01
 
-no_quarter = int(input("how many quarters?: "))
-no_dimes = int(input("how many dimes?: "))
-no_nickel = int(input("how many nickel?: "))
-no_pennies = int(input("how many pennies?: "))
+should_stop = False
 
-def money_count():
+while not should_stop:
 
-    cal = (
-        quarter * no_quarter
-        + dimes * no_dimes
-        + nickel * no_nickel
-        + pennies * no_pennies
-    )
-    return cal
+    drink = input("What would you like? (espresso/latte/cappuccino):")
+    if drink == "report":
+        format_water = f"Water: {resources['water']}ml \nMilk: {resources['milk']}ml \ncoffee: {resources['coffee']}ml \nMoney: {resources['money']}"
+        print(format_water)
+        should_stop = True
+
+    elif drink == "espresso" or drink == "cappuccino" or drink == "latte":
+
+        def order(drink):
+            water = MENU[drink]["ingredients"]["water"]
+            if drink == "cappuccino" or drink == "latte":
+                milk = MENU[drink]["ingredients"]["milk"]
+            else:
+                milk = 0
+            coffee = MENU[drink]["ingredients"]["coffee"]
+            cost = MENU[drink]["cost"]
+
+            return water, milk, coffee, cost
+
+        water_drink = order(drink)[0]
+        milk_drink = order(drink)[1]
+        coffee_drink = order(drink)[2]
+        cost_drink = order(drink)[3]
+
+        def check_order(water, milk, coffee):
+            if water > resources["water"]:
+                print("you don't have enough water")
+                return False
+            elif milk > resources["milk"]:
+                print("you don't have enough milk")
+                return False
+
+            elif coffee > resources["coffee"]:
+                print("you don't have enough coffee")
+                return False
+
+            else:
+                resources["water"] -= water
+                resources["milk"] -= milk
+                resources["coffee"] -= coffee
+                return True
+
+        if check_order(water_drink, milk_drink, coffee_drink):
+            print(coin)
+            print("please insert a coin first")
+
+            no_quarter = int(input("how many quarters?: "))
+            no_dimes = int(input("how many dimes?: "))
+            no_nickel = int(input("how many nickel?: "))
+            no_pennies = int(input("how many pennies?: "))
+
+            cal = (
+                quarter * no_quarter
+                + dimes * no_dimes
+                + nickel * no_nickel
+                + pennies * no_pennies
+            )
+
+            your_change = cal - cost_drink
+            if your_change <= 0:
+                print("you don't have enough money")
+                should_stop = True
+            elif your_change >= cost_drink:
+                resources["money"] += cost_drink
+                print(f"Your total cost is: ${cost_drink:.2f}")
+                print(f"Your change ${your_change:.2f} ")
+                print(f"Thank you for ordering {drink}")
+        else:
+            should_stop = True
+    else:
+        print("Invalid data")
+        should_stop = True
